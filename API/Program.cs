f71591d51dcd90192c8bb595ba6727f3762412ad
INFOR\rdongre <robby.dongre@infor.com>
2026-09-02 22:59:35 +0530

    chore(setup): initialize solution structure and gitignore

---
+++ API/Program.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    );
+});
+
+// Learn more about configuring OpenAPI
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
